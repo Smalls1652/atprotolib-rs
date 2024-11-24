524a769dadb74a248abdda951f44c92c073389d8
Timothy Small <smalls.online@icloud.com>
2024-11-24 00:58:55 -0500

    Add docs for app.bsky.video

--- atprotolib-rs/src/types/app_bsky/video/defs.rs
+++ atprotolib-rs/src/types/app_bsky/video/defs.rs
@@ -1,33 +1,34 @@
 use serde::{Deserialize, Serialize};
 
-/*    Type: jobStatus
-    Id: app.bsky.video.defs#jobStatus
-    Kind: object
-
-    Properties:
-    - job_id: string (JsonProperty: jobId) [Required]
-    - did: string (JsonProperty: did) [Required]
-    - state: string (JsonProperty: state) [Required]
-    - progress: integer  (JsonProperty: progress) [Optional]
-    - blob: blob  (JsonProperty: blob) [Optional]
-    - error: string (JsonProperty: error) [Optional]
-    - message: string (JsonProperty: message) [Optional]
-*/
+/// Represents the status of a video upload job.
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "$type", rename = "app.bsky.video.defs#jobStatus")]
 pub struct JobStatus {
+    /// The ID of the job.
     #[serde(rename = "jobId")]
     pub job_id: String,
+
+    /// The DID of the job.
     #[serde(rename = "did")]
     pub did: String,
+
+    /// The state of the job.
     #[serde(rename = "state")]
     pub state: String,
+
+    /// The progress of the job.
     #[serde(rename = "progress", default)]
     pub progress: i32,
+
+    /// The blob of the job.
     #[serde(rename = "blob", skip_serializing_if = "Option::is_none")]
     pub blob: Option<Vec<u8>>,
+
+    /// The error of the job.
     #[serde(rename = "error", skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
+
+    /// The message of the job.
     #[serde(rename = "message", skip_serializing_if = "Option::is_none")]
     pub message: Option<String>
 }
